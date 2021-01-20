8e9e2ff704512adbddd0006f38ba331be420c24e
Miriam <miriamhinojo@stilografica.es>
2021-01-20 08:45:57 +0100

    Nivell1 - Exercici2

--- js/main.js
+++ js/main.js
@@ -43,3 +43,15 @@ const greet = who => {
 //Compruebo que funciona
 greet("Marc");
 
+//Exercici 2: Arregla l'error
+var users = 
+[{ firstName: 'Homer', lastName: 'Simpson' },
+{ firstName: 'Marge', lastName: 'Simpson' },
+{ firstName: 'Bart', lastName: 'Simpson' },
+{ firstName: 'Lisa', lastName: 'Simpson' },
+{ firstName: 'Maggie', lastName: 'Simpson' }];
+
+users.map(function (user) {
+    //Incorporo el console log para ver que funciona
+    console.log(user.firstName);
+});
